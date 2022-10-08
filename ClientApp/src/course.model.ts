e84e62ac7cea81271d29d026f61be44605dfb695
ihebabbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-10-08 19:37:30 +0200

    Create course.model.ts

---
+++ ClientApp/src/course.model.ts
@@ -0,0 +1,11 @@
+export interface course{
+    id:number;
+    name:string;
+    author:string;
+    duration: number;
+    type: string
+    price: number;
+    ratings: number;
+    image:string;
+    description:string;
+}
