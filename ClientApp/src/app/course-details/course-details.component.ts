7363bfcd802a507bd02c1025378caf4f32106ffc
ihebabbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-10-11 15:42:20 +0200

    Update course-details.component.ts

--- ClientApp/src/app/course-details/course-details.component.ts
+++ ClientApp/src/app/course-details/course-details.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import{course} from '../course.model'
-import{CoursesService} from './../courses.service'
+import { course } from '../course.model';
+import { CoursesService } from './../courses.service';
 import { ActivatedRoute, ParamMap, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
-  styleUrls: ['./course-details.component.css']
+  styleUrls: ['./course-details.component.css'],
 })
 export class CourseDetailsComponent implements OnInit {
   selectedCourse: any;
 
   Course: course;
-  constructor(private coursesService:CoursesService ,    private activatedRoute: ActivatedRoute,private route: ActivatedRoute,private router: Router) { }
+  constructor(
+    private coursesService: CoursesService,
+    private activatedRoute: ActivatedRoute,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe({
@@ -21,19 +26,12 @@ export class CourseDetailsComponent implements OnInit {
         //console.log(this.selectedCourse , p.get('id')); // p['id]
       },
     });
-
-     
   }
 
-
- 
-
-
-
-  getbyid(id:any){
-  this.coursesService.getCourseByID(id).subscribe((data: course)=>{
-    this.Course = data;
-    console.log('this.Course', this.Course)
-  });
+  getbyid(id: any) {
+    this.coursesService.getCourseByID(id).subscribe((data: course) => {
+      this.Course = data;
+      console.log('this.Course', this.Course);
+    });
   }
 }
