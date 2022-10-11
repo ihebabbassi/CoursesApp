e92d84033d771814998502b3e45ad2e5bae1afee
ihebabbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-10-11 15:41:13 +0200

    Update app-routing.module.ts

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import {AddCourseComponent} from './add-course/add-course.component';
 import {CourseDetailsComponent} from './course-details/course-details.component';
-
+import {BookCourseComponent} from './book-course/book-course.component'
 
 const routes: Routes = [
   {path: 'courses', component: CoursesComponent},
   {path: 'addcourse', component:AddCourseComponent},
-  {path: 'coursedetail/:id', component:CourseDetailsComponent}
+  {path: 'coursedetail/:id', component:CourseDetailsComponent},
+  {path: 'book', component:BookCourseComponent}
 
 
 ];
